e54c3d03a24aa8eaf9032a87816051507c3f8456
vijoh01 <vijoh01@gmail.com>
2022-09-19 02:19:51 +0200

    dynamic items

--- scripts/common.js
+++ scripts/common.js
@@ -11,3 +11,24 @@ for (var i = 0; i < button.length; i++) {
         }
     });
 }
+
+var productDiv = document.createElement('div');
+productDiv.classList.add('product-item');
+
+
+document.querySelector('.add-item').addEventListener('click', addProduct);
+let containerDiv = document.querySelector('.product-container');
+containerDiv.appendChild(productDiv);
+var i = 0;
+function addProduct() {
+    
+    let productDetails = `<div class="product-item">
+    <img src="https://images.footway.com/02/60786-15_001.png?auto=format&fit=max&w=450" alt="">
+    </div>`;
+    if (i >= 1) {
+        productDiv.insertAdjacentHTML('afterend', productDetails);
+    } else {
+    productDiv.insertAdjacentHTML('beforeend', productDetails);
+    }
+    i++;
+}
