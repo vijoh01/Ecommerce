da7564025c961b75db800989b9aa1e75c347c450
vijoh01 <vijoh01@gmail.com>
2022-09-20 19:45:18 +0200

    register

--- scripts/common.js
+++ scripts/common.js
@@ -19,16 +19,19 @@ productDiv.classList.add('product-item');
 document.querySelector('.add-item').addEventListener('click', addProduct);
 let containerDiv = document.querySelector('.product-container');
 containerDiv.appendChild(productDiv);
+
 var i = 0;
 function addProduct() {
-    
+
     let productDetails = `<div class="product-item">
     <img src="https://placedog.net/450/450?random" alt="">
     </div>`;
     if (i >= 1) {
         productDiv.insertAdjacentHTML('afterend', productDetails);
+        
     } else {
     productDiv.insertAdjacentHTML('beforeend', productDetails);
+    
     }
     i++;
 }
@@ -42,4 +45,28 @@ document.querySelector('.fi-bs-user').addEventListener('click', function() {
     } else {
         login.style.display = 'flex';
     }
+});
+
+
+document.addEventListener('mouseup', function(e) {
+    var login = document.querySelector('.login');
+    var register = document.querySelector('.register');
+    if (!(login.contains(e.target) || register.contains(e.target))) {
+        login.style.display = 'none';
+        register.style.display = 'none';
+    }
+});
+  
+
+var registerbtn = document.querySelector('.register-btn');
+
+registerbtn.addEventListener('click', function() {
+    document.querySelector('.login').style.display = 'none';
+    document.querySelector('.register').style.display = 'flex';
+    document.querySelector('.register').style.maxHeight = '22rem';
+});
+
+document.querySelector('.login-btn').addEventListener('click', function() {
+    document.querySelector('.register').style.display = 'none';
+    document.querySelector('.login').style.display = 'flex';
 });
